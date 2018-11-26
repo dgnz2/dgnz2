595991108ed95fa1fbf4e657391d1998d0d66ad4
dgnz <zedign@gmail.com>
2018-11-26 10:54:14 -0500

    Adding Pages

---
+++ common/script.js
@@ -0,0 +1,187 @@
+//////////// funcs //////////////
+function zas_opts(a) {
+	if (a == 1) return '<option>Poster</option> <option>Bag</option> <option>T-Shirt</option> ';
+	if (a == 2) return '<option>Oscar-Claude Monet - The Paintings</option> <option>Édouard Manet - The Paintings</option> <option>Paul Gauguin - Paintings & Drawings</option> <option>Amedeo Modigliani - The Paintings</option> <option>Rembrandt van Rijn - Paintings & Drawings</option> <option>JMW Turner - Paintings & Drawings Vol 1</option> <option>JMW Turner - Paintings & Drawings Vol 2</option> <option>JMW Turner - Paintings & Drawings Vol 3</option> <option>JMW Turner - Paintings & Drawings Vol 4</option> <option>Francisco Goya - The Paintings</option> <option>Berthe Morisot - Paintings & Drawings</option> <option>Albert Bierstadt - The Paintings</option> <option>Joaquín Sorolla - The Paintings</option> <option>Caspar David Friedrich - Paintings & Drawings</option> <option>Thomas Eakins - Paintings & Drawings Vol 1</option> <option>Thomas Eakins - Paintings & Drawings Vol 2</option> <option>John William Waterhouse - Paintings & Drawings</option> <option>William Merritt Chase - The Paintings</option> <option>William-Adolphe Bouguereau - Paintings & Drawings</option> <option>Peter Paul Rubens - Paintings & Drawings Vol 1</option> <option>Peter Paul Rubens - Paintings & Drawings Vol 2</option> <option>Michelangelo Merisi da Caravaggio - The Paintings</option> <option>Diego Velázquez - The Paintings</option> <option>Vincent Van Gogh - The Paintings Vol 1</option> <option>Vincent Van Gogh - The Paintings Vol 2</option> <option>Vincent Van Gogh - The Paintings Vol 3</option> <option>Paul Cézanne - The Paintings Vol 1</option> <option>Paul Cézanne - The Paintings Vol 2</option> <option>Odilon Redon - Paintings & Drawings Vol 1</option> <option>Odilon Redon - Paintings & Drawings Vol 2</option> <option>Odilon Redon - Paintings & Drawings Vol 3</option> <option>Alfred Sisley - The Paintings Vol 1</option> <option>Alfred Sisley - The Paintings Vol 2</option> <option>Ilya Repin - Paintings & Drawings Vol 1</option> <option>Ilya Repin - Paintings & Drawings Vol 2</option> <option>Pierre-Auguste Renoir - The Paintings Vol 1</option> <option>Pierre-Auguste Renoir - The Paintings Vol 2</option> <option>Pierre-Auguste Renoir - The Paintings Vol 3</option> <option>Pierre-Auguste Renoir - The Paintings Vol 4</option> <option>Pierre-Auguste Renoir - The Paintings Vol 5</option> <option>Pierre-Auguste Renoir - The Paintings Vol 6</option> <option>Camille Pissarro - The Paintings Vol 1</option> <option>Camille Pissarro - The Paintings Vol 2</option> <option>Camille Pissarro - The Paintings Vol 3</option> <option>Camille Pissarro - The Paintings Vol 4</option> <option>Eugène Boudin - Paintings & Drawings Vol 1</option> <option>Eugène Boudin - Paintings & Drawings Vol 2</option> <option>John Everett Millais - Paintings & Drawings</option> <option>Edward Burne-Jones - Paintings & Drawings</option> <option>Julian Onderdonk - The Paintings</option> <option>John Singer Sargent - Paintings & Drawings Vol 1</option> <option>John Singer Sargent - Paintings & Drawings Vol 2</option> <option>Edgar Degas - Paintings & Drawings Vol 1</option> <option>Edgar Degas - Paintings & Drawings Vol 2</option> <option>Mary Cassatt - Paintings & Drawings</option> <option>Angelica Kauffman - Paintings & Drawings</option> <option>Katsushika Hokusai - Paintings & Drawings Vol 1</option> <option>Katsushika Hokusai - Paintings & Drawings Vol 2</option> <option>Katsushika Hokusai - Paintings & Drawings Vol 3</option> <option>Henri de Toulouse-Lautrec - Paintings & Drawings</option> <option>Anders Zorn - Paintings & Drawings Vol 1</option> <option>Anders Zorn - Paintings & Drawings Vol 2</option> <option>Henri-Edmond Cross - Paintings & Drawings</option> <option>Raphael - Paintings & Drawings</option> <option>Eugène Delacroix - Paintings & Drawings Vol 1</option> <option>Eugène Delacroix - Paintings & Drawings Vol 2</option> <option>Rosalba Carriera - The Paintings</option> <option>Childe Hassam - Paintings & Drawings Vol 1</option> <option>Childe Hassam - Paintings & Drawings Vol 2</option> <option>Childe Hassam - Paintings & Drawings Vol 3</option> <option>Zhao Mengfu - Paintings & Drawings</option> <option>Paula Modersohn-Becker - Paintings & Drawings</option> <option>Utagawa Hiroshige - Paintings & Drawings Vol 1</option> <option>Utagawa Hiroshige - Paintings & Drawings Vol 2</option> <option>Dante Gabriel Rossetti - Paintings & Drawings</option> <option>Johannes Vermeer - The Paintings</option> <option>Albrecht Dürer - Paintings & Drawings Vol 1</option> <option>Albrecht Dürer - Paintings & Drawings Vol 2</option> <option>Artemisia Gentileschi - The Paintings</option> <option>Henri Lebasque - Paintings & Drawings Vol 1</option> <option>Henri Lebasque - Paintings & Drawings Vol 2</option>';
+}
+
+function detectmob() {
+	// v3 
+	// BS STANDARD: >= 768 -sm- , >= 998 -md-
+	if (window.innerWidth < 768) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function viewport(percentage, property) {
+	// v2 (vmax) - returns viewport % in pixels
+	// property='vw','vh','vmax', usage: viewport(40, "vh")+'px';
+	var w = Math.round((Math.max(document.documentElement.clientWidth, window.innerWidth || 0)) * percentage / 100);
+	var h = Math.round((Math.max(document.documentElement.clientHeight, window.innerHeight || 0)) * percentage / 100);
+	if (property == "vw") {
+		return w;
+	}
+	if (property == "vh") {
+		return h;
+	}
+	if (property == "vmax") {
+		if (w > h) {
+			return w;
+		}
+		if (h > w) {
+			return h;
+		}
+		if (w == h) {
+			return w;
+		}
+	}
+}
+
+function ga_evCatVal(evCat, evVal) {
+	// v2
+	// console.log(evCat + ' ' + evVal);  // KEEP!
+	try {
+		ga('send', 'event', evCat, evVal, {
+			'nonInteraction': 1
+		});
+	} catch (a) {
+		//
+	}
+}
+/////////// /funcs //////////////
+////////////////////////////////////////////////////
+///////////////////    ORDER FORM   //////////////////////
+////////////////////////////////////////////////////
+if (zdsite == "orderform") {
+	$('body').append('<div id="fdbk" style="box-shadow: 0 0 5px #555555;opacity:0.75;background-color: #008000; bottom: 30px; font: 12px/1em sans-serif; padding: 10px; position: fixed; right: 30px;"><a id="fdbk_btn" style="font-size:18px;font-weight:bold;text-decoration:none;color:#fff;display:block;height:100%;width:100%;cursor:pointer">Open Order Form</a></div>');
+	////////////// <GDBSFRM 1/2 :: LINK> //////////////////////// 
+	// v2: now fdbk_openClose(url) 
+	// req detectmob(), viewport(), ga_evCatVal(), JQ 
+	// -------------- VARS TO CHANGE -------------
+	var gdbsfrm_url = '../common/c/?s=fdbk';
+	var clc_elem = '#fdbk, .fdbk'; // string of onclick elem for jq obj e.g. '#abc' or '.abc'
+	var scrolling = "yes"; // 'yes'|'no'
+	var gaCat = 'fdbk'; // ga categ for ga_evCatVal
+	var gaVal = 'inf_fdbk_btn: N:'; // ga val for ga_evCatVal
+	// / -------------- VARS TO CHANGE -------------
+	function fdbk_openClose(url) {
+		if (document.getElementById('fdbk_window')) {
+			$('.fdbk_window').remove();
+		} else {
+			var dimn = (detectmob()) ? 80 : 70;
+			$('body').append(
+				'<div id="fdbk_window" class="fdbk_window" style="width:' + viewport(dimn, 'vw') + 'px;height:' + viewport(dimn, 'vh') + 'px;background-color:#aaa;position:fixed;left:50%;top:50%;margin-top:-' + (viewport(dimn, 'vh') / 2) + 'px;margin-left:-' + (viewport(dimn, 'vw') / 2) + 'px;outline:solid 3px #aaa;box-shadow:0 0 10px #000;z-index:2147483647" >' +
+				'<div style="width:20px;height:20px;position:absolute;right:0;color:white;cursor:pointer;" id="fdbk_close"><img style="width:100%;height:100%" src="https://www.google.com/intl/en_us/mapfiles/close.gif"/></div>' +
+				'<iframe style="height:100%;width:100%;display:block;background:white;" src="' + url + '" scrolling="' + scrolling + '" frameborder="0" border="0" ></iframe>' +
+				'<div>'
+			);
+			$("#fdbk_close").on('click', function() {
+				$('.fdbk_window').remove();
+				$('.fdbk_window').hide();
+			});
+		}
+	}
+	$(clc_elem).on('click', function() {
+		ga_evCatVal(gaCat, gaVal);
+		fdbk_openClose(gdbsfrm_url);
+		// console.log('tada');
+	});
+	////////////// </GDBSFRM 1/2 :: LINK> //////////////////////// 
+}
+////////////// </GDBSFRM 2/2 :: FORM> //////////////////////// 
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+if (zdsite == "dyn_catcher") {
+	////////////// <GDBSFRM 2/2 :: FORM> //////////////////////// 
+	// v2
+	// req: ga_evCatVal(), JQ
+	// 
+	// / -------------- VARS TO CHANGE -------------
+	var gd_id = '1FAIpQLSdgIW9TUMy7Xg34_Qco9eE_CeiR30CqJ2f8QA1xbZYFvoerYQ';
+	var singleinputid = "1946163534";
+	var div_id = 'crecdiv'; // receiving div, eg 'crecdiv' for /c/
+	var require_bootstrap = "yes"; // "yes"|"no"; if bootstrap not already available.
+	var form_headline = '<div style="font-size:20px;line-height:2.5em;"> <!-- <img style="height:1em;display:inline-block;vertical-align:middle;" src="" /> --> Order an image for product </div> ';
+	var thankyou_headline = '<h2>Thank You!</h2> <h3>Your request has been sent.</h3>';
+	var form_elements = '' + // bs form markup
+		// 
+		'<div class="form-group">' +
+		'<label for="product_type">Product:</label> <select class="form-control" id="id_product_type">' +
+		zas_opts(1) +
+		'</select>' +
+		'</div>' +
+		'<div class="form-group">' +
+		'<label for="book_name">From the Monograph:</label> <select class="form-control" id="id_book_name"> ' +
+		zas_opts(2) +
+		// '<option>1</option> <option>2</option> <option>3</option> <option>4</option> '+
+		'</select>' +
+		'</div>' +
+		'<div class="form-group">' +
+		'	<label>Page number &amp; title of image <small>(from monograph above)</small></label> <input required pattern=".{3,}" title="Image Number" class="form-control" type="text" name="image_number" value="" id="id_image_number"/>' +
+		'</div>' +
+		'<div class="form-group">' +
+		'	<label>Notify me on this email</label> <input required pattern=".{3,}" title="Your email" class="form-control" type="text" name="your_email" value="" id="id_your_email"/>' +
+		'</div>' +
+		'';
+	// 
+	function valdfdbk() { // use this genric valdfdbk(), or cstmize it accrdngly
+		// 1st validate fields
+		// ...
+		// 2nd, once validated, combine all from display from to make final single-input to send...
+		var txt2send =
+			' Product: ' + document.forms["fdbk_frm_name"]["id_product_type"].value +
+			' Book: ' + document.forms["fdbk_frm_name"]["id_book_name"].value +
+			' Image: ' + document.forms["fdbk_frm_name"]["id_image_number"].value +
+			' Email: ' + document.forms["fdbk_frm_name"]["id_your_email"].value + '';
+		$('#entry_' + singleinputid).val(txt2send);
+		$("#fdbk").css({
+			'display': 'none'
+		});
+		setTimeout(
+			function() {
+				$("#" + div_id).append('<div class="container">' + thankyou_headline + '</div>');
+			}, 1000);
+		return true;
+	}
+	// / -------------- VARS TO CHANGE -------------
+	// 
+	// 
+	var a = require_bootstrap == "yes" ? '<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>' : '';
+	document.getElementsByTagName('head')[0].insertAdjacentHTML("beforeend", a);
+	// 
+	function GDBSForm() {
+		// combine display form's inputs into one to send
+		// 
+		$("#" + div_id).html(
+			'<iframe name="fdbk_hidden_iframe" id="fdbk_hidden_iframe" style="display:none;" onload=""></iframe>' +
+			'<div class="container" id="fdbk" style="margin-bottom:10px">' +
+			form_headline +
+			// 
+			// 
+			'\x3C\x66\x6F\x72\x6D\x20\x61\x63\x74\x69\x6F\x6E\x3D\x22\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x6F\x63\x73\x2E\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D\x2F\x66\x6F\x72\x6D\x73\x2F\x64\x2F\x65\x2F' +
+			gd_id +
+			'/formResponse" method="post" ' +
+			' onSubmit="return valdfdbk()" ' +
+			' role="form" name="fdbk_frm_name" id="fdbk_frm_id" target="fdbk_hidden_iframe" >' +
+			form_elements +
+			'<input type="hidden" name="entry.' + singleinputid + '" id="entry_' + singleinputid + '" value=""/>' +
+			'	<input class="btn btn-default" type="submit" name="submit" value="Submit"/>' +
+			'</form>' +
+			'</div>' +
+			''
+		);
+	}
+	if (require_bootstrap == "yes") {
+		$.getScript("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js")
+			.done(function(script, textStatus) {
+				GDBSForm();
+			})
+			.fail(function(jqxhr, settings, exception) {
+				// $("div.log").text("Triggered ajaxError handler.");
+			});
+	} else {
+		GDBSForm();
+	}
+}
